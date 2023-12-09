310e15a9fa09f5263f5d9cc57aaa03e85023e160
Ahmed Barakat <ahmedfbarakat575@gmail.com>
2023-12-09 13:23:50 +0200

    Add project files.

---
+++ algoriza-internship-2023BE108.Data/MetaData/Router.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoriza_internship_2023BE108.Data.MetaData
+{
+    public class Router
+    {
+        public const string root = "api/";
+        public const string version = "v1/";
+        public const string rule = root + version;
+
+        public static class DoctorRouting
+        {
+            public const string prefix = rule + "doctor/";
+            public const string list = prefix + "list";
+            public const string GetById = prefix + "{id}";
+
+
+
+        }
+    }
+}
---
+++ algoriza-internship-2023BE108.Infrastructure/InfrastructureDependencies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoriza_internship_2023BE108.Infrastructure
+{
+    public class InfrastructureDependencies
+    {
+    }
+}
